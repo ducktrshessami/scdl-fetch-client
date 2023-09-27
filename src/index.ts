d0b1520cdc85750ddb7dc89f0dfff56f5e436991
ducktrshessami <jwee761@gmail.com>
2023-09-27 15:37:00 -0700

    refactor: method files

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-export async function fetchClientID(): Promise<string> {
-
-}
-
-export async function testClientID(clientID: string): Promise<boolean> {
-    const res = await fetch(`https://api-v2.soundcloud.com/resolve?client_id=${clientID}`, { method: "HEAD" });
-    return res.status === 200 || res.status === 404;
-}
+export * from "./fetch";
+export * from "./test";
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export async function testClientID(clientID: string): Promise<boolean> {
+    const res = await fetch(`https://api-v2.soundcloud.com/resolve?client_id=${clientID}`, { method: "HEAD" });
+    return res.status === 200 || res.status === 404;
+}
