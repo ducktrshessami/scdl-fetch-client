3945ecb843d7b9439150f1ebfec7c54192ae9e75
ducktrshessami <cal.tf2.3@gmail.com>
2025-06-17 15:09:40 -0700

    refactor: ClientFetchError name

--- src/error.ts
+++ src/error.ts
@@ -1,6 +1,6 @@
 export class ClientFetchError extends Error {
     constructor(message: string) {
         super(message);
-        this.name = "FetchClientError";
+        this.name = "ClientFetchError";
     }
 }
